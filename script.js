f745449a30039a7f32a3495c880b3b86322df860
Tisha Maharjan <tishamaharjan.13@gmail.com>
2025-11-11 07:53:59 +0545

    week1

---
+++ script.js
@@ -0,0 +1,57 @@
+function changeBackground() {
+  const group = document.getElementById("group");
+  group.style.backgroundColor = "#3b82f6";
+}
+
+function changeContent() {
+  document.getElementById("group1").innerHTML = "Hi Everyone!!";
+}
+
+function changeImage() {
+  const img = document.getElementById("images");
+  img.src = "images/1.jpg";
+}
+
+function addNumbers() {
+  let x, y, z;
+  x = 5;
+  y = 10;
+  z = x + y;
+  document.getElementById("addition").innerHTML = `Addition ${x}+${y}: ` + z;
+}
+
+function subtractNumbers() {
+  let x, y, z;
+  x = 10;
+  y = 5;
+  z = x - y;
+  document.getElementById("subtraction").innerHTML =
+    `Subtraction ${x}-${y}: ` + z;
+}
+
+function multiplyNumbers() {
+  let x, y, z;
+  x = 5;
+  y = 4;
+  z = x * y;
+  document.getElementById("multiplication").innerHTML =
+    `Multiplication ${x}*${y}: ` + z;
+}
+
+function divideNumbers() {
+  let x, y, z;
+  x = 20;
+  y = 4;
+  z = x / y;
+  document.getElementById("division").innerHTML = `Division ${x}/${y}: ` + z;
+}
+
+function checkOddEven() {
+  let number = document.getElementById("num").value;
+
+  if (number % 2 == 0) {
+    document.getElementById("result").innerHTML = number + " is Even.";
+  } else {
+    document.getElementById("result").innerHTML = number + " is Odd.";
+  }
+}
